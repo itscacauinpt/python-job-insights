2fdbb3e08bfbd3163f16a854e63225f2638aa79e
cacau <anaclaudia.sza@outlook.com>
2022-12-08 18:52:26 -0300

    feat: implement get_max_salary and get_min_salary functions

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,5 +1,7 @@
 from typing import Union, List, Dict
 
+from src.insights.jobs import read
+
 
 def get_max_salary(path: str) -> int:
     """Get the maximum salary of all jobs
@@ -16,7 +18,21 @@ def get_max_salary(path: str) -> int:
     int
         The maximum salary paid out of all job opportunities
     """
-    raise NotImplementedError
+    try:
+        jobs = read(path)
+        salaries = [
+            int(job['max_salary'])
+            for job in jobs
+            if job['max_salary'].isdigit()
+            ]
+
+        max_salary = max(salaries)
+
+    except OSError:
+        raise Exception('Error system')
+
+    else:
+        return max_salary
 
 
 def get_min_salary(path: str) -> int:
@@ -34,7 +50,21 @@ def get_min_salary(path: str) -> int:
     int
         The minimum salary paid out of all job opportunities
     """
-    raise NotImplementedError
+    try:
+        jobs = read(path)
+        salaries = [
+            int(job['min_salary'])
+            for job in jobs
+            if job['min_salary'].isdigit()
+            ]
+
+        min_salary = min(salaries)
+
+    except OSError:
+        raise Exception('Error system')
+
+    else:
+        return min_salary
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
