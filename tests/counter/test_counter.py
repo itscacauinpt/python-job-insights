c2b8b1e2a1d097478130d0bfbab943dd35ffe558
cacau <anaclaudia.sza@outlook.com>
2022-12-12 16:23:17 -0300

    feat: add test count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,7 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = 'data/jobs.csv'
+    word = 'Backend'
+    assert count_ocurrences(path, word) == 55
