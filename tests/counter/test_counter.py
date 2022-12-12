347a53c94ef159cced8e6e7903eac567ce0713c8
cacau <anaclaudia.sza@outlook.com>
2022-12-12 16:28:33 -0300

    fix: delete comment and unnecessary lines

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,6 +2,4 @@ from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    path = 'data/jobs.csv'
-    word = 'Backend'
-    assert count_ocurrences(path, word) == 55
+    assert count_ocurrences('data/jobs.csv', 'Backend') == 55
