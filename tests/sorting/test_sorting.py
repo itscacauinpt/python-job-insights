b129ad7c2e95f2f40e4dc176e874a653390fdfb8
cacau <anaclaudia.sza@outlook.com>
2022-12-12 19:40:45 -0300

    feat: add test sort_by_criteria max_salary

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,87 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+
+jobs_fake = [
+        {
+            'title': 'Web developer',
+            'min_salary': '3000',
+            'max_salary': '7000',
+            'date_posted': '2020-05-08'
+        },
+        {
+            'title': 'Front end developer',
+            'min_salary': '1000',
+            'max_salary': '5000',
+            'date_posted': '2020-05-10'
+        },
+        {
+            'title': 'Back end developer',
+            'min_salary': '1000',
+            'max_salary': '3000',
+            'date_posted': '2020-02-05'
+        },
+        {
+            'title': 'Full stack end developer',
+            'min_salary': '4000',
+            'max_salary': '8000',
+            'date_posted': '2020-05-01'
+        }
+    ]
+
+mock_max_salary = [
+        {
+            'title': 'Full stack end developer',
+            'min_salary': '4000',
+            'max_salary': '8000',
+            'date_posted': '2020-05-01'
+        },
+        {
+            'title': 'Web developer',
+            'min_salary': '3000',
+            'max_salary': '7000',
+            'date_posted': '2020-05-08'
+        },
+        {
+            'title': 'Front end developer',
+            'min_salary': '1000',
+            'max_salary': '5000',
+            'date_posted': '2020-05-10'
+        },
+        {
+            'title': 'Back end developer',
+            'min_salary': '1000',
+            'max_salary': '3000',
+            'date_posted': '2020-02-05'
+        }
+    ]
+
+# mock_date_posted = [
+#         {
+#             'title': 'Back end developer',
+#             'min_salary': '1000',
+#             'max_salary': '3000',
+#             'date_posted': '2020-02-05'
+#         },
+#         {
+#             'title': 'Full stack end developer',
+#             'min_salary': '4000',
+#             'max_salary': '8000',
+#             'date_posted': '2020-05-01'
+#         },
+#         {
+#             'title': 'Web developer',
+#             'min_salary': '3000',
+#             'max_salary': '7000',
+#             'date_posted': '2020-05-08'
+#         },
+#         {
+#             'title': 'Front end developer',
+#             'min_salary': '1000',
+#             'max_salary': '5000',
+#             'date_posted': '2020-05-10'
+#         }
+#     ]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(jobs_fake, 'max_salary')
+    assert jobs_fake == mock_max_salary
